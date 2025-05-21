64d2cc8b9013053f70acabac83ae5d266173394c
Sandu Guzun <sanduguzun762@gmail.com>
2025-05-21 18:50:40 +0300

    sa

---
+++ src/test/java/com/example/My/secret/project/MySecretProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My.secret.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecretProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
